526c7abef1dc640a2be5e120c53956a650beba34
Tim Biden <timbiden@gmail.com>
2019-04-10 12:50:55 -0700

    Getting keycode but not value from HTML.

---
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener("keydown", function(e) {
+  console.log("Event is " + e.keyCode);
+
+  const audio = document.querySelector(`audio[data-key=${e.keyCode}]`);
+  console.log(audio);
+});
