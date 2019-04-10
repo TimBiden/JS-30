664a4fc3446013ba2ab35b2406e658e806046f1a
Tim Biden <timbiden@gmail.com>
2019-04-10 14:09:59 -0700

    Removes 'playing' class. My Way.

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,3 +1,5 @@
+let keyValue = 0;
+
 window.addEventListener("keydown", function(e) {
   //Get key code data
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -6,8 +8,16 @@ window.addEventListener("keydown", function(e) {
   // If not a valid key, escape function
   if (!audio) return;
 
+  keyValue = key;
   audio.currentTime = 0; // Reset play time to beginning
   audio.play(); // Play audio
 
   key.classList.add("playing");
 });
+
+function removeTransition(e) {
+  keyValue.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
