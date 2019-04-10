44c7d2fe1053a2df94e1ef17b2b59977d46d3621
Tim Biden <timbiden@gmail.com>
2019-04-10 13:21:16 -0700

    currentTime reset.

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,8 +1,8 @@
 window.addEventListener("keydown", function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if (!audio) {
-    return;
-  }
-  console.log(audio);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
   audio.play();
 });
