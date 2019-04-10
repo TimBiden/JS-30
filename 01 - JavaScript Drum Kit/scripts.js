9ef70ac1f37f21f0c44fe151b7f25c1ff97f98f5
Tim Biden <timbiden@gmail.com>
2019-04-10 13:30:52 -0700

    Get key code daya and add class "playing"

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,8 +1,13 @@
 window.addEventListener("keydown", function(e) {
+  //Get key code data
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+  // If not a valid key, escape function
   if (!audio) return;
 
-  audio.currentTime = 0;
-  audio.play();
+  audio.currentTime = 0; // Reset play time to beginning
+  audio.play(); // Play audio
+
+  key.classList.add("playing");
 });
